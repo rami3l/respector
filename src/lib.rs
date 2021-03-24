ccb1bb8d1bd2addd8a1141d6a1b9f9701caf7b94
rami3l <rami3l@outlook.com>
2021-03-25 00:45:38 +0100

    feat: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,79 @@
+//! A helper to add inspection to Option and Result types.
+
+pub use self::prelude::*;
+
+mod prelude {
+    pub trait OptionInspector<T> {
+        /// Does something with the contained [`Some`] value element of an [`Option`], passing the value on.
+        ///
+        /// # Examples
+        /// ```rust
+        /// use respector::OptionInspector;
+        ///
+        /// assert_eq!(Some(10).inspect(|x| println!("Some({})", x)), Some(10)); // Prints `Some(10)`.
+        /// assert_eq!(None::<i32>.inspect(|x| println!("Some({})", x)), None); // Prints nothing.
+        /// ```
+        fn inspect<F: FnMut(&T)>(self, f: F) -> Option<T>;
+    }
+
+    impl<T> OptionInspector<T> for Option<T> {
+        fn inspect<F: FnMut(&T)>(self, mut f: F) -> Option<T> {
+            self.map(|it| {
+                f(&it);
+                it
+            })
+        }
+    }
+
+    pub trait ResultInspector<T, E> {
+        /// Does something with the contained [`Ok`] value element of a [`Result`], passing the value on.
+        ///
+        /// # Examples
+        /// ```rust
+        /// use respector::ResultInspector;
+        ///
+        /// assert_eq!(
+        ///     Ok::<_, ()>(10).inspect(|x| println!("Ok({})", x)),
+        ///     Ok::<_, ()>(10)
+        /// ); // Prints `Ok(10)`.
+        /// assert_eq!(
+        ///     Err::<(), _>(10).inspect(|x| println!("Ok({:?})", x)),
+        ///     Err::<(), _>(10)
+        /// ); // Prints nothing.
+        /// ```
+        fn inspect<F: FnMut(&T)>(self, f: F) -> Result<T, E>;
+
+        /// Does something with the contained [`Err`] value element of a [`Result`], passing the value on.
+        ///
+        /// # Examples
+        /// ```rust
+        /// use respector::ResultInspector;
+        ///
+        /// assert_eq!(
+        ///     Err::<(), _>(10).inspect_err(|x| println!("Err({})", x)),
+        ///     Err::<(), _>(10)
+        /// ); // Prints `Err(10)`.
+        /// assert_eq!(
+        ///     Ok::<_, ()>(10).inspect_err(|x| println!("Err({:?})", x)),
+        ///     Ok::<_, ()>(10)
+        /// ); // Prints nothing.
+        /// ```
+        fn inspect_err<F: FnMut(&E)>(self, f: F) -> Result<T, E>;
+    }
+
+    impl<T, E> ResultInspector<T, E> for Result<T, E> {
+        fn inspect<F: FnMut(&T)>(self, mut f: F) -> Result<T, E> {
+            self.map(|it| {
+                f(&it);
+                it
+            })
+        }
+
+        fn inspect_err<F: FnMut(&E)>(self, mut f: F) -> Result<T, E> {
+            self.map_err(|err| {
+                f(&err);
+                err
+            })
+        }
+    }
+}
