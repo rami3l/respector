640b719a6b5217d2c928357e747ade9a391aaeaa
rami3l <rami3l@outlook.com>
2021-04-13 13:14:44 +0200

    feat: support `no_std`

--- README.md
+++ README.md
@@ -1,29 +1,26 @@
 # respector
 
-![Downloads](https://img.shields.io/crates/d/respector)
-![License](https://img.shields.io/crates/l/respector)
+[![Downloads](https://img.shields.io/crates/d/respector)](https://crates.io/crates/respector)
+[![License](https://img.shields.io/crates/l/respector)](./LICENSE)
 [![crates.io](https://img.shields.io/crates/v/respector?logo=rust)](https://crates.io/crates/respector)
 [![docs.rs](https://docs.rs/respector/badge.svg)](https://docs.rs/respector)
 
 An extension to add [`inspect`](https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.inspect) method to `Option` and `Result` types.
+
+This allows doing something with the value contained in a `Some`, an `Ok` (with `inspect`) or an `Err` (with `inspect_err`) while passing the value on,
+which can be useful for introducing side effects in debugging, logging, etc.
 
 ## Usage
 
 ```rust
 use respector::prelude::*;
 
-assert_eq!(
-    Some(10).inspect(|x| println!("Some({})", x)),
-    Some(10)
-); // Prints `Some(10)`.
+let some = Some(10);
+assert_eq!(some.inspect(|x| println!("Some({})", x)), some); // Prints `Some(10)`.
 
-assert_eq!(
-    Ok::<_, ()>(10).inspect(|x| println!("Ok({})", x)),
-    Ok(10)
-); // Prints `Ok(10)`.
+let ok = Ok::<_, ()>(10);
+assert_eq!(ok.inspect(|x| println!("Ok({})", x)), ok); // Prints `Ok(10)`.
 
-assert_eq!(
-    Err::<(), _>(10).inspect_err(|x| println!("Err({})", x)),
-    Err(10)
-); // Prints `Err(10)`.
+let err = Err::<(), _>(10);
+assert_eq!(Err(10).inspect_err(|x| println!("Err({})", x)), err); // Prints `Err(10)`.
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
 //! An extension to add [`inspect`](https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.inspect) method to [`Option`] and [`Result`] types.
+//! This allows doing something with the value contained in a `Some`, an `Ok` (with `inspect`) or an `Err` (with `inspect_err`) while passing the value on,
+//! which can be useful for introducing side effects in debugging, logging, etc.
+//!
+//!
+//! ## Examples
+//! ```rust
+//! use respector::prelude::*;
+//!
+//! let some = Some(10);
+//! assert_eq!(some.inspect(|x| println!("Some({})", x)), some); // Prints `Some(10)`.
+//!
+//! let ok = Ok::<_, ()>(10);
+//! assert_eq!(ok.inspect(|x| println!("Ok({})", x)), ok); // Prints `Ok(10)`.
+//!
+//! let err = Err::<(), _>(10);
+//! assert_eq!(Err(10).inspect_err(|x| println!("Err({})", x)), err); // Prints `Err(10)`.
+//! ```
+
+#![no_std]
 
 pub use self::prelude::*;
 
@@ -9,8 +28,10 @@ pub mod prelude {
         /// # Examples
         /// ```rust
         /// use respector::prelude::*;
-        /// assert_eq!(Some(10).inspect(|x| println!("Some({})", x)), Some(10)); // Prints `Some(10)`.
-        /// assert_eq!(None::<i32>.inspect(|x| println!("Some({})", x)), None); // Prints nothing.
+        ///
+        /// let (some, none) = (Some(10), None::<i32>);
+        /// assert_eq!(some.inspect(|x| println!("Some({})", x)), some); // Prints `Some(10)`.
+        /// assert_eq!(none.inspect(|x| println!("Some({})", x)), none); // Prints nothing.
         /// ```
         fn inspect<F: FnMut(&T)>(self, f: F) -> Option<T>;
     }
@@ -30,14 +51,10 @@ pub mod prelude {
         /// # Examples
         /// ```rust
         /// use respector::prelude::*;
-        /// assert_eq!(
-        ///     Ok::<_, ()>(10).inspect(|x| println!("Ok({})", x)),
-        ///     Ok(10)
-        /// ); // Prints `Ok(10)`.
-        /// assert_eq!(
-        ///     Err::<(), _>(10).inspect(|x| println!("Ok({:?})", x)),
-        ///     Err(10)
-        /// ); // Prints nothing.
+        ///
+        /// let (ok, err) = (Ok::<_, ()>(10), Err::<(), _>(10));
+        /// assert_eq!(ok.inspect(|x| println!("Ok({})", x)), ok); // Prints `Ok(10)`.
+        /// assert_eq!(err.inspect(|x| println!("Ok({:?})", x)), err); // Prints nothing.
         /// ```
         fn inspect<F: FnMut(&T)>(self, f: F) -> Result<T, E>;
 
@@ -46,14 +63,10 @@ pub mod prelude {
         /// # Examples
         /// ```rust
         /// use respector::prelude::*;
-        /// assert_eq!(
-        ///     Err::<(), _>(10).inspect_err(|x| println!("Err({})", x)),
-        ///     Err(10)
-        /// ); // Prints `Err(10)`.
-        /// assert_eq!(
-        ///     Ok::<_, ()>(10).inspect_err(|x| println!("Err({:?})", x)),
-        ///     Ok(10)
-        /// ); // Prints nothing.
+        ///
+        /// let (ok, err) = (Ok::<_, ()>(10), Err::<(), _>(10));
+        /// assert_eq!(err.inspect_err(|x| println!("Err({})", x)), err); // Prints `Err(10)`.
+        /// assert_eq!(ok.inspect_err(|x| println!("Err({:?})", x)), ok); // Prints nothing.
         /// ```
         fn inspect_err<F: FnMut(&E)>(self, f: F) -> Result<T, E>;
     }
